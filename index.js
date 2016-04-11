47d5de4179535b47b0c5da6e76cd7fc9c81cb16f
Hossam Hammady <github@hammady.net>
2016-04-11 11:52:39 +0300

    migrating templates

--- index.js
+++ index.js
@@ -25,29 +25,47 @@ var addListEntry = function(listType, entry) {
   });    
 }
 
-var importList = function(listType, addCallback){
+var importList = function(listType, addCallback) {
   mandrill_source[listType].list({}, function(result) {
-    console.log(listType, "Found", result.length, " entries");
-    _.each(result, function(entry){
-      addCallback(entry)
-    })
+    console.log(listType, "Found", result.length, "entries");
+    _.each(result, addCallback)
   }, function(e) {
     console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message)
     process.exit(1)
   });
 }
 
-var importWhitelist = function(){
+var importWhitelist = function() {
   importList('whitelists', function(entry){
     addListEntry('whitelists', {email: entry.email, comment: entry.detail})
   })
 }
 
-var importBlacklist = function(){
+var importBlacklist = function() {
   importList('rejects', function(entry){
     addListEntry('rejects', {email: entry.email, comment: entry.reason, subaccount: entry.subaccount})
   })
 }
 
+var addTemplate = function(template) {
+  mandrill_target.templates.add(_.merge(template, {publish: !!template.published_at}), function(result) {
+    console.log("templates", result.slug, result.name);
+  }, function(e) {
+    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message)
+    process.exit(1)
+  });
+}
+
+var importTemplates = function() {
+  mandrill_source.templates.list({}, function(result) {
+    console.log("templates", "Found", result.length, "entries");
+    _.each(result, addTemplate)
+  }, function(e) {
+    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+    process.exit(1)
+  });
+}
+
 importWhitelist();
 importBlacklist();
+importTemplates();
